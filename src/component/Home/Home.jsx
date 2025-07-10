e3547a03d512622d7d34b265baab795afb4ad485
Saugat Rana <ranasaugat9@gmail.com>
2025-07-09 23:44:05 -0400

    Working on Components

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import logo from '../../Images/Logo.png'
+import logo from '../../Images/Me.png'
 function Home() {
   return (
     <>
-    <div className='flex'>
-       <h1>Hello, I am Saugat Rana and I am a Software Developer</h1>
+    <div className='grid grid-cols-1 md:grid-cols-2 items-center font-winky bg-[#95ebd4]'>
+      <div>
+        <h1 className=' text-white md:text-center'>Hello, I am Saugat Rana </h1>
+        </div>
+       
        <div>
-        <img src={logo} alt="" />
-        <div className='flex justify-evenly'> 
-        <button>Connect with me </button>
-        <button>My Resume</button>
+        <img src={logo} alt="Image" className="h-100 md:h-auto mx-auto" />
+        <div className='flex justify-evenly items-center'> 
+        <button className='rounded-3xl bg-[#00585E] px-5 py-2 border border-white text-white' > About me </button>
+        <button className='rounded-3xl bg-[#00585E] px-5 py-2 border border-white text-white'>Resume</button>
         </div>
         
        </div>
