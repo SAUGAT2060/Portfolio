1a067a959c89a25ebe40e0545f4480ef0571ca3c
Saugat Rana <ranasaugat9@gmail.com>
2025-07-11 20:32:28 -0400

    About section

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 function About() {
   return (
-    <div>This is the About Page</div>
+    <div className='bg-linear-to-tr from-[#95ebd4] to-[#00585E] font-winky'>
+
+
+      <div className='py-3'>
+        <h1 className=' text-white text-center font-semibold text-shadow-lg text-5xl'>About</h1>
+      </div>
+      <div  className='md:flex'>
+        <div className='text-center px-15 py-10 md:px-10py-10'>
+ <p className=' text-shadow-2xs text-shadow-white  '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla corrupti suscipit facere, et blanditiis libero odio neque cumque eveniet. Vitae nulla sapiente accusamus placeat aliquid! Temporibus soluta beatae nesciunt veritatis.</p>
+        </div>
+       
+        <div className='text-center px-15 py-5 md:px-10,py-5'>
+   <p className='text-center text-shadow-2xs text-shadow-white mt-4 md-min-w-[90]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla corrupti suscipit facere, et blanditiis libero odio neque cumque eveniet. Vitae nulla sapiente accusamus placeat aliquid! Temporibus soluta beatae nesciunt veritatis.</p>
+        </div>
+       
+        
+      </div>
+    </div>
   )
 }
 
--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -3,9 +3,10 @@ import logo from '../../Images/Me.png'
 function Home() {
   return (
     <>
-    <div className='grid grid-cols-1 md:grid-cols-2 items-center font-winky bg-[#95ebd4]'>
+    <div className='grid grid-cols-1 md:grid-cols-2 items-center font-winky  bg-linear-to-br from-[#00585E] to-[#95ebd4]'>
       <div>
-        <h1 className=' text-white md:text-center'>Hello, I am Saugat Rana </h1>
+        <h1 className=' text-white text-center text-5xl text-shadow-lg font-semibold'>Hello, I am Saugat Rana </h1>
+        <p className=' text-2xl text-shadow-2xs text-center text-shadow-white font-medium'>I am Front End Developer from Nepal </p>
         </div>
        
        <div>
